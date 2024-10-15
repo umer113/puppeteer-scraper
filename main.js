7be786c49a16df6e393de0a2276b7644f00757ee
Shaikh Umer Qadir <145275785+umer113@users.noreply.github.com>
2024-10-16 00:48:03 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,226 +1,185 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs");
-const XLSX = require("xlsx");
-const path = require("path");
+const puppeteer = require('puppeteer');
+const xlsx = require('xlsx');
+const fs = require('fs');
+const path = require('path');
 
 (async () => {
-    const cookiesPath = "cookies.json";
-    const properties = []; // Array to hold all property details
-    const baseUrls = [
-        'https://www.infocasas.com.bo/alquiler'
+    const browser = await puppeteer.launch({ headless: false });
+
+    const urlsToScrape = [
+        'https://www.propertyfinder.eg/en/search?c=1&t=1&pf=5500000&pt=6500000&fu=0&ob=mr',
+        // Add more URLs as needed
     ];
 
-    const browser = await puppeteer.launch({
-        headless: 'new', 
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-blink-features=AutomationControlled",
-            "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
-        ],
-        defaultViewport: null,
-    });
+    const scrapePropertyData = async (url, browser) => {
+        try {
+            const propertyPage = await browser.newPage();
+            await propertyPage.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
 
-    const page = await browser.newPage();
+            let transaction_type = '';
 
-    await page.setExtraHTTPHeaders({
-        "Accept-Language": "en-US,en;q=0.9",
-    });
-
-    if (fs.existsSync(cookiesPath)) {
-        const cookies = JSON.parse(fs.readFileSync(cookiesPath, "utf8"));
-        await page.setCookie(...cookies);
-        console.log("Cookies loaded.");
-    }
-
-    await page.evaluateOnNewDocument(() => {
-        Object.defineProperty(navigator, "webdriver", { get: () => undefined });
-        Object.defineProperty(navigator, "platform", { get: () => "Win32" });
-        Object.defineProperty(navigator, "plugins", {
-            get: () => [
-                {
-                    description: "Portable Document Format",
-                    filename: "internal-pdf-viewer",
-                    length: 1,
-                    name: "Chrome PDF Plugin",
-                },
-                {
-                    description: "Portable Document Format",
-                    filename: "mhjfbmdgcfjbbpaeojofohoefgiehjai",
-                    length: 1,
-                    name: "Chrome PDF Viewer",
-                },
-            ],
-        });
-        Object.defineProperty(navigator, "languages", {
-            get: () => ["en-US", "en"],
-        });
-    });
-
-    try {
-        for (const baseUrl of baseUrls) {
-            await page.goto(baseUrl, { waitUntil: 'networkidle2', timeout: 0 });
-
-            const totalPages = await page.evaluate(() => {
-                const paginationLinks = Array.from(document.querySelectorAll('ul.search-results-pagination li a.ant-pagination-item-link'));
-
-                if (paginationLinks.length > 0) {
-                    const pageNumbers = paginationLinks.map(link => {
-                        const href = link.getAttribute('href');
-                        const match = href.match(/pagina(\d+)/);
-                        return match ? parseInt(match[1], 10) : null;
-                    }).filter(num => num !== null);
-
-                    return Math.max(...pageNumbers);
-                }
-
-                return 1;
-            });
-
-            console.log(`Total pages: ${totalPages}`);
-
-            const propertyUrls = new Set();
-            for (let i = 1; i <= totalPages; i++) {
-                console.log(`Scraping page ${i} of ${baseUrl}...`);
-                await page.goto(`${baseUrl}/pagina${i}`, { waitUntil: 'networkidle2', timeout: 0 });
-
-                const urls = await page.evaluate(() => {
-                    const anchors = Array.from(document.querySelectorAll('a.lc-cardCover'));
-                    return anchors.map(anchor => anchor.href);
-                });
-
-                urls.forEach(url => propertyUrls.add(url));
+            if (url.includes('sale')) {
+                transaction_type = 'sale';
+            } else if (url.includes('rent')) {
+                transaction_type = 'rent';
             }
 
-            console.log(`Total unique property URLs for ${baseUrl}: ${propertyUrls.size}`);
+            await propertyPage.waitForSelector('script#__NEXT_DATA__');
 
-            let count = 1;
+            const propertyData = await propertyPage.evaluate((transaction_type) => {
+                const scriptTag = document.querySelector('script#__NEXT_DATA__');
+                let propertyDetails = {};
 
-            for (const url of propertyUrls) {
-                const ipUrlPattern = /^https?:\/\/\d+\.\d+\.\d+\.\d+/;
-                if (ipUrlPattern.test(url)) {
-                    console.log(`Skipping IP URL: ${url}`);
-                    continue;
-                }
+                if (scriptTag) {
+                    const jsonData = JSON.parse(scriptTag.innerText);
+                    const property = jsonData.props.pageProps.propertyResult.property;
 
-                console.log(`Scraping property at URL: ${url}`);
-                await page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
-
-                const propertyDetails = await page.evaluate((url) => {
-                    const name = document.querySelector('meta[property="og:title"]')?.content || '';
-                    const description = document.querySelector('.ant-typography.property-description')?.innerText || '';
-                    const addressElements = Array.from(document.querySelectorAll('.property-location-tag p'));
-                    const address = addressElements.map(p => p.innerText.trim()).join(', ') || '';
-
-                    let price = document.querySelector('.ant-typography.price strong')?.innerText || '';
-
-                    const geoJson = document.querySelector('script[type="application/ld+json"]')?.innerText || '{}';
-                    const geo = JSON.parse(geoJson).object?.geo || {};
-                    const latitude = geo.latitude || '';
-                    const longitude = geo.longitude || '';
-
-                    const detailRows = Array.from(document.querySelectorAll('.jsx-952467510.technical-sheet .ant-row'));
-                    const details = {};
-                    let propertyType = '';
-                    let area = '';
-
-                    detailRows.forEach(row => {
-                        const keyElement = row.querySelector('.ant-space-item span.ant-typography:not(.ant-typography-secondary)');
-                        const valueElement = row.querySelector('strong') || row.querySelector('span:not(.ant-typography-secondary)');
-
-                        const key = keyElement?.innerText?.trim();
-                        const value = valueElement?.innerText?.trim();
-
-                        if (key && value) {
-                            details[key] = value;
-
-                            if (key === 'Tipo de Propiedad') {
-                                propertyType = value;
+                    const characteristics = {};
+                    const characteristicsContainer = document.querySelector('.styles_desktop_list__Kq7ZK');
+                    if (characteristicsContainer) {
+                        const items = characteristicsContainer.querySelectorAll('.styles_desktop_list__item__lF_Fh');
+                        items.forEach(item => {
+                            const label = item.querySelector('.styles_desktop_list__label-text__0YJ8y')?.innerText.trim();
+                            const value = item.querySelector('.styles_desktop_list__value__uIdMl')?.innerText.trim();
+                            if (label && value) {
+                                characteristics[label] = value;
                             }
-
-                            if (key === 'M² edificados') {
-                                area = value;
-                            }
-                        }
-                    });
-
-                    let transactionType = '';
-                    const transactionElement = document.querySelector('.ant-typography.ant-typography-secondary.operation_type');
-                    if (transactionElement && transactionElement.innerText.includes('Venta')) {
-                        transactionType = 'sale';
-                    } else {
-                        transactionType = 'rent';
+                        });
                     }
 
-                    if (price == '') {
-                        price = 'ask';
+                    const amenities = [];
+                    const amenitiesContainer = document.querySelector('.styles_amenity__container__kL4sm');
+                    if (amenitiesContainer) {
+                        const items = amenitiesContainer.querySelectorAll('.styles_amenity__c2P5u');
+                        items.forEach(item => {
+                            const amenity = item.querySelector('p.styles_text__IlyiW')?.innerText.trim();
+                            if (amenity) {
+                                amenities.push(amenity);
+                            }
+                        });
                     }
 
-                    return {
-                        url,
-                        name,
-                        description,
-                        address,
-                        price,
-                        propertyType,
-                        area,
-                        transactionType,
-                        latitude,
-                        longitude,
-                        details
-                    };
-                }, url);
-
-                const flattenedDetails = Object.assign({}, propertyDetails.details);
-                const completeDetails = { ...propertyDetails, ...flattenedDetails };
-                delete completeDetails.details;
-
-                console.log(completeDetails);
-                properties.push(completeDetails);
-                console.log("count: ", count);
-                count++;
-            }
-
-            const outputDir = path.join(__dirname, 'output');
-            if (!fs.existsSync(outputDir)) {
-                fs.mkdirSync(outputDir);
-            }
-
-            const workbook = XLSX.utils.book_new();
-            const worksheet = XLSX.utils.json_to_sheet(properties);
-
-            const range = XLSX.utils.decode_range(worksheet['!ref']);
-            for (let C = range.s.c; C <= range.e.c; ++C) {
-                const cell = worksheet[XLSX.utils.encode_cell({ r: 0, c: C })];
-                if (cell && cell.s) {
-                    cell.s = {
-                        font: {
-                            bold: true,
-                        }
+                    propertyDetails = {
+                        name: property.title,
+                        address: property.location.full_name,
+                        price: property.price.value,
+                        description: property.description,
+                        area: property.size.value,
+                        propertyType: property.property_type,
+                        transactionType: transaction_type,
+                        latitude: property.location.coordinates.lat,
+                        longitude: property.location.coordinates.lon,
+                        propertyUrl: property.share_url,
+                        characteristics: characteristics,
+                        amenities: amenities
                     };
                 }
-            }
 
-            XLSX.utils.book_append_sheet(workbook, worksheet, "Properties");
+                return propertyDetails;
+            }, transaction_type);
 
-            const safeUrl = baseUrl.replace(/[^a-z0-9]/gi, '_').toLowerCase();
-            const outputPath = path.join(outputDir, `${safeUrl}.xlsx`);
+            await propertyPage.close();
+            return propertyData;
+        } catch (error) {
+            console.error(`Error scraping property data: ${error}`);
+            return null;
+        }
+    };
 
-            XLSX.writeFile(workbook, outputPath);
+    const saveToExcel = (data, url) => {
+        const formattedData = data.map(property => {
+            return {
+                ...property,
+                characteristics: JSON.stringify(property.characteristics), // Convert object to JSON string
+                amenities: property.amenities.join(', ') // Convert array to comma-separated string
+            };
+        });
 
-            if (fs.existsSync(outputPath)) {
-                console.log(`File ${outputPath} created successfully.`);
-            } else {
-                console.error(`File ${outputPath} not found.`);
-            }
+        const ws = xlsx.utils.json_to_sheet(formattedData);
+        const wb = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(wb, ws, "Properties");
+
+        // Ensure the output directory exists
+        const outputDir = path.join(__dirname, 'output');
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir);
         }
 
-        const cookies = await page.cookies();
-        fs.writeFileSync(cookiesPath, JSON.stringify(cookies, null, 2));
-        console.log("Cookies saved.");
-    } catch (error) {
-        console.error("Error during scraping:", error);
-    } finally {
-        await browser.close();
+        const fileName = path.join(outputDir, url.replace(/[^a-z0-9]/gi, '_').toLowerCase() + '.xlsx');
+        xlsx.writeFile(wb, fileName);
+        console.log(`Data saved to ${fileName}`);
+    };
+
+    for (const url of urlsToScrape) {
+        console.log(`Scraping URL: ${url}`);
+
+        try {
+            const page = await browser.newPage();
+            await page.goto(url, { waitUntil: 'networkidle2', timeout: 600000 });
+
+            const numberOfPropertiesResult = await page.evaluate(() => {
+                const span = document.querySelector('span[aria-label="Search results count"]');
+                if (span) {
+                    const propertiesText = span.innerText;
+                    const numberOfProperties = parseInt(propertiesText.replace(/,/g, '').split(' ')[0]);
+                    return {
+                        spanText: span.innerText,
+                        numberOfProperties: numberOfProperties
+                    };
+                }
+                return { spanText: null, numberOfProperties: null };
+            });
+
+            const numberOfProperties = numberOfPropertiesResult.numberOfProperties;
+
+            if (numberOfProperties !== null) {
+                console.log(`Number of properties: ${numberOfProperties}`);
+
+                const totalPages = Math.ceil(numberOfProperties / 27);
+                console.log(`Total number of pages: ${totalPages}`);
+
+                let allPropertyUrls = [];
+
+                for (let i = 1; i <= totalPages; i++) {
+                    try {
+                        await page.goto(`${url}&page=${i}`, { waitUntil: 'networkidle2', timeout: 600000 });
+                        await page.waitForSelector('a.link-module_link__TaDrq.styles_desktop_gallery-item-wrapper__OW7RH');
+
+                        const propertyUrls = await page.evaluate(() => {
+                            return Array.from(document.querySelectorAll('a.link-module_link__TaDrq.styles_desktop_gallery-item-wrapper__OW7RH'))
+                                .map(link => link.href);
+                        });
+
+                        console.log(`Page ${i}: Found ${propertyUrls.length} property URLs`);
+                        allPropertyUrls = allPropertyUrls.concat(propertyUrls);
+                    } catch (error) {
+                        console.error(`Error navigating to page ${i}: ${error}`);
+                    }
+                }
+
+                console.log(`Total number of property URLs: ${allPropertyUrls.length}`);
+
+                let scrapedData = [];
+
+                for (const propertyUrl of allPropertyUrls) {
+                    console.log(`Visiting: ${propertyUrl}`);
+                    const data = await scrapePropertyData(propertyUrl, browser);
+                    if (data) {
+                        console.log(data);
+                        scrapedData.push(data);
+                    }
+                }
+
+                saveToExcel(scrapedData, url);
+            } else {
+                console.log('Span element not found');
+            }
+
+            await page.close();
+        } catch (error) {
+            console.error(`Error processing URL ${url}: ${error}`);
+        }
     }
+
+    await browser.close();
 })();
