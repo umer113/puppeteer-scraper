2769b6364d4c257503144305c3fec69d646972fd
Shaikh Umer Qadir <145275785+umer113@users.noreply.github.com>
2024-12-03 19:31:13 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const puppeteer = require('puppeteer-core');
-const puppeteerExtra = require('puppeteer-extra');
+const puppeteer = require('puppeteer-extra');
 const stealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
 const ExcelJS = require('exceljs');
